e4c21c102c683e358b1f3db9a739fd88447cc837
babyboy <bugs.bunny.shah@gmail.com>
2022-02-06 15:50:38 -0600

    testsuite_fixes

--- startFetcher.sh
+++ startFetcher.sh
@@ -1,8 +1,7 @@
-curl --location --request POST 'http://localhost/dataIngester/fetch/' \
+curl --location --request POST 'http://localhost:8080/dataIngester/fetch/' \
 --header 'Principal: PAlDekAoo0XWjAicU9SQDKgy7B0y2p2t' \
 --header 'Bearer: bearer' \
 --header 'Content-Type: application/json' \
---header 'Cookie: authentik_csrf=mobQCxHGKsIbdXS0AFmigH5yJQ2URucYpwumrxBl5FVeLiwBQ68nkRa7RnPfBr5q' \
 --data-raw '{
   "agentId": "ian",
   "entity": "flight"
