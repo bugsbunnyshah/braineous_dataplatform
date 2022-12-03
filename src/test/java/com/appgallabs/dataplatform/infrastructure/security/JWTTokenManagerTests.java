189bca1a30b1d17f570615a4809562e9a937286b
babyboy <bugs.bunny.shah@gmail.com>
2022-12-02 21:17:18 -0600

    authentication_flow: component_tests

--- src/test/java/com/appgallabs/dataplatform/infrastructure/security/JWTTokenManagerTests.java
+++ src/test/java/com/appgallabs/dataplatform/infrastructure/security/JWTTokenManagerTests.java
@@ -1,5 +1,6 @@
 package com.appgallabs.dataplatform.infrastructure.security;
 
+import com.appgallabs.dataplatform.infrastructure.Http;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.response.Response;
 import org.apache.commons.io.IOUtils;
@@ -8,6 +9,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 
 import static io.restassured.RestAssured.given;
@@ -20,6 +25,9 @@ public class JWTTokenManagerTests {
 
     @Inject
     private JWTTokenManager jwtTokenManager;
+
+    @Inject
+    private Http http;
 
     @Test
     public void issueServiceComponent() throws Exception{
@@ -38,5 +46,117 @@ public class JWTTokenManagerTests {
         String generatedToken = response.body().toString();
         assertEquals(200, response.getStatusCode());
         assertNotNull(generatedToken);
+    }
+
+    @Test
+    public void testAuthenticationMissingToken() throws Exception{
+        String restUrl = "http://localhost:8080/data/security/issue";
+
+        //Create the Experiment
+        HttpClient httpClient = http.getHttpClient();
+
+        HttpRequest.Builder httpRequestBuilder = HttpRequest.newBuilder();
+        HttpRequest httpRequest = httpRequestBuilder.uri(new URI(restUrl))
+                .GET()
+                .build();
+
+
+        HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        String responseJson = httpResponse.body();
+        int status = httpResponse.statusCode();
+        int expectedStatus = 200;
+        assertEquals(expectedStatus,status);
+
+        String generatedToken = responseJson;
+        logger.info(generatedToken);
+
+        String endpoint = "http://localhost:8080/data/microservice";
+        httpRequestBuilder = HttpRequest.newBuilder();
+        httpRequest = httpRequestBuilder.uri(new URI(endpoint))
+                .GET()
+                .build();
+
+
+        httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        responseJson = httpResponse.body();
+        status = httpResponse.statusCode();
+        expectedStatus = 401;
+        assertEquals(expectedStatus,status);
+    }
+
+    @Test
+    public void testAuthenticationSuccess() throws Exception{
+        String restUrl = "http://localhost:8080/data/security/issue";
+
+        //Create the Experiment
+        HttpClient httpClient = http.getHttpClient();
+
+        HttpRequest.Builder httpRequestBuilder = HttpRequest.newBuilder();
+        HttpRequest httpRequest = httpRequestBuilder.uri(new URI(restUrl))
+                .GET()
+                .build();
+
+
+        HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        String responseJson = httpResponse.body();
+        int status = httpResponse.statusCode();
+        int expectedStatus = 200;
+        assertEquals(expectedStatus,status);
+
+        String generatedToken = responseJson;
+        logger.info(generatedToken);
+
+        String endpoint = "http://localhost:8080/data/microservice";
+        httpRequestBuilder = HttpRequest.newBuilder();
+        httpRequest = httpRequestBuilder.uri(new URI(endpoint))
+                .header("Authorization", "Bearer "+generatedToken)
+                .GET()
+                .build();
+
+
+        httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        responseJson = httpResponse.body();
+        status = httpResponse.statusCode();
+        expectedStatus = 200;
+        assertEquals(expectedStatus,status);
+        logger.info(responseJson);
+    }
+
+    @Test
+    public void testAuthenticationFailed() throws Exception{
+        String restUrl = "http://localhost:8080/data/security/issue";
+
+        //Create the Experiment
+        HttpClient httpClient = http.getHttpClient();
+
+        HttpRequest.Builder httpRequestBuilder = HttpRequest.newBuilder();
+        HttpRequest httpRequest = httpRequestBuilder.uri(new URI(restUrl))
+                .GET()
+                .build();
+
+
+        HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        String responseJson = httpResponse.body();
+        int status = httpResponse.statusCode();
+        int expectedStatus = 200;
+        assertEquals(expectedStatus,status);
+
+        String generatedToken = responseJson;
+        logger.info(generatedToken);
+
+        String endpoint = "http://localhost:8080/data/microservice";
+        httpRequestBuilder = HttpRequest.newBuilder();
+        httpRequest = httpRequestBuilder.uri(new URI(endpoint))
+                .header("Authorization", "Bearer "+generatedToken)
+                .GET()
+                .build();
+
+
+        httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        responseJson = httpResponse.body();
+        status = httpResponse.statusCode();
+        expectedStatus = 200;
+        assertEquals(expectedStatus,status);
+        logger.info(responseJson);
     }
 }
