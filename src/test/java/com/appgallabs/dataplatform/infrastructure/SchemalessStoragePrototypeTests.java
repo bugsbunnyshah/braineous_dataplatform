09284bb1e3d1a993b18112f874bfa0a0aa6a8474
babyboy <bugs.bunny.shah@gmail.com>
2023-06-18 17:28:06 -0400

    schemaless_ingestion: storage_prototype (start)

---
+++ src/test/java/com/appgallabs/dataplatform/infrastructure/SchemalessStoragePrototypeTests.java
@@ -0,0 +1,16 @@
+package com.appgallabs.dataplatform.infrastructure;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@QuarkusTest
+public class SchemalessStoragePrototypeTests {
+    private static Logger logger = LoggerFactory.getLogger(SchemalessStoragePrototypeTests.class);
+
+    @Test
+    public void prototypeStoreFlatJson() throws Exception{
+
+    }
+}
