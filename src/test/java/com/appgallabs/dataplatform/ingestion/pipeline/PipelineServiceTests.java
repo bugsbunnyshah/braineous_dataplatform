6722f58c1f6f5fce922d3e3a4e7d57ba441731db
babyboy <bugs.bunny.shah@gmail.com>
2023-08-12 09:25:26 -0400

    flink_based_data_processor: ingestion_pipeline_productization

--- src/test/java/com/appgallabs/dataplatform/ingestion/pipeline/PipelineServiceTests.java
+++ src/test/java/com/appgallabs/dataplatform/ingestion/pipeline/PipelineServiceTests.java
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 //TODO
-//@QuarkusTest
+@QuarkusTest
 public class PipelineServiceTests {
     private static Logger logger = LoggerFactory.getLogger(PipelineServiceTests.class);
 
