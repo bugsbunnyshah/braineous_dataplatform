48235cc5765d79c629986b46d01ed409b4e5f984
babyboy <bugs.bunny.shah@gmail.com>
2023-09-03 19:53:47 -0400

    kafka_service: integration_with_braineous_pipeline_service

--- src/test/java/com/appgallabs/dataplatform/infrastructure/kafka/EventProcessorTests.java
+++ src/test/java/com/appgallabs/dataplatform/infrastructure/kafka/EventProcessorTests.java
@@ -1,8 +1,11 @@
 package com.appgallabs.dataplatform.infrastructure.kafka;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
+import com.google.gson.JsonParser;
 import io.quarkus.test.junit.QuarkusTest;
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 
 import org.slf4j.Logger;
@@ -10,6 +13,9 @@ import org.slf4j.LoggerFactory;
 import org.testng.annotations.BeforeTest;
 
 import javax.inject.Inject;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -37,6 +43,27 @@ public class EventProcessorTests {
 
         for(int i=0; i<1; i++) {
             JsonObject response = this.eventProcessor.processEvent(json);
+
+            logger.info("*****************");
+            logger.info(response.toString());
+            logger.info("*****************");
+
+            assertNotNull(response);
+        }
+
+        Thread.sleep(3000);
+    }
+
+    @Test
+    public void processEventWithPipeline() throws Exception {
+        String jsonString = IOUtils.toString(Thread.currentThread().
+                        getContextClassLoader().getResourceAsStream("ingestion/algorithm/input_array.json"),
+                StandardCharsets.UTF_8
+        );
+        JsonArray jsonArray = JsonParser.parseString(jsonString).getAsJsonArray();
+
+        for(int i=0; i<1; i++) {
+            JsonObject response = this.eventProcessor.processEvent(jsonArray);
 
             logger.info("*****************");
             logger.info(response.toString());
