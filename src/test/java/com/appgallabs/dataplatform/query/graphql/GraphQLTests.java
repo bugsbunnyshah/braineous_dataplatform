857544ba784673bd2f55223a2bfcf01805f7022a
babyboy <bugs.bunny.shah@gmail.com>
2023-09-24 16:01:01 -0400

    graphql: prototype

--- src/test/java/com/appgallabs/dataplatform/query/graphql/GraphQLTests.java
+++ src/test/java/com/appgallabs/dataplatform/query/graphql/GraphQLTests.java
@@ -1,16 +1,28 @@
 package com.appgallabs.dataplatform.query.graphql;
 
-import com.appgallabs.dataplatform.util.JsonUtil;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
-import io.quarkus.test.junit.QuarkusTest;
+
 import org.apache.commons.io.IOUtils;
+
+import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.given;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.appgallabs.dataplatform.util.JsonUtil;
 
 
 @QuarkusTest
@@ -19,10 +31,29 @@ public class GraphQLTests {
 
     @Test
     public void testAllProducts() throws Exception{
+        String restUrl = "http://localhost:8080/graphql/";
+
         String queryJson = IOUtils.toString(Thread.currentThread().getContextClassLoader().
                         getResourceAsStream("graphql/datafetcher.json"),
                 StandardCharsets.UTF_8);
         JsonElement jsonElement = JsonParser.parseString(queryJson);
         JsonUtil.printStdOut(jsonElement);
+
+        String input = jsonElement.toString();
+
+        HttpClient httpClient = HttpClient.newBuilder().build();
+        HttpRequest.Builder httpRequestBuilder = HttpRequest.newBuilder();
+        HttpRequest httpRequest = httpRequestBuilder.uri(new URI(restUrl))
+                .POST(HttpRequest.BodyPublishers.ofString(input))
+                .build();
+
+
+        HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        String responseJson = httpResponse.body();
+        int statusCode = httpResponse.statusCode();
+        assertEquals(200, statusCode);
+
+        JsonElement responseJsonElement = JsonParser.parseString(responseJson);
+        JsonUtil.printStdOut(responseJsonElement);
     }
 }
