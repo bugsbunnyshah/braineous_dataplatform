977546838f08542dde60e6ee74c2a91e8e340b53
babyboy <bugs.bunny.shah@gmail.com>
2023-10-04 22:05:45 -0400

    pipeline_management: pipeService

--- src/main/java/com/appgallabs/dataplatform/pipeline/manager/service/DataCleansingService.java
+++ src/main/java/com/appgallabs/dataplatform/pipeline/manager/service/DataCleansingService.java
@@ -1,4 +1,27 @@
 package com.appgallabs.dataplatform.pipeline.manager.service;
 
+import com.appgallabs.dataplatform.pipeline.manager.model.DataCleanerFunction;
+import com.appgallabs.dataplatform.pipeline.manager.model.Pipe;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public class DataCleansingService {
+
+    private static Logger logger = LoggerFactory.getLogger(DataCleansingService.class);
+
+
+    public void addCleanerFunction(Pipe pipe, DataCleanerFunction dataCleanerFunction){
+
+    }
+
+    public void removeCleanerFunction(Pipe pipe, DataCleanerFunction dataCleanerFunction){
+
+    }
+
+    public void removeAllCleanerFunctions(Pipe pipe){
+
+    }
 }
--- src/main/java/com/appgallabs/dataplatform/pipeline/manager/service/PipelineMonitoringService.java
+++ src/main/java/com/appgallabs/dataplatform/pipeline/manager/service/PipelineMonitoringService.java
@@ -1,4 +1,14 @@
 package com.appgallabs.dataplatform.pipeline.manager.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public class PipelineMonitoringService {
+
+    private static Logger logger = LoggerFactory.getLogger(PipelineMonitoringService.class);
+
+
 }
