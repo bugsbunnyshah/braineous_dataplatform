7615f7fa631bc87c4b903665cf35f1794201d8cf
babyboy <bugs.bunny.shah@gmail.com>
2023-10-16 10:24:28 -0400

    pipeline_management: pipeline_storage_components

--- src/main/java/com/appgallabs/dataplatform/pipeline/manager/service/SubscriptionService.java
+++ src/main/java/com/appgallabs/dataplatform/pipeline/manager/service/SubscriptionService.java
@@ -1,33 +1,75 @@
 package com.appgallabs.dataplatform.pipeline.manager.service;
 
+import com.appgallabs.dataplatform.infrastructure.MongoDBJsonStore;
+import com.appgallabs.dataplatform.infrastructure.PipelineStore;
+import com.appgallabs.dataplatform.infrastructure.Tenant;
 import com.appgallabs.dataplatform.pipeline.manager.model.Subscription;
+import com.appgallabs.dataplatform.preprocess.SecurityTokenContainer;
+import com.google.gson.JsonArray;
+import com.mongodb.client.MongoClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
-import java.util.List;
+import javax.inject.Inject;
 
 @ApplicationScoped
 public class SubscriptionService {
     private static Logger logger = LoggerFactory.getLogger(SubscriptionService.class);
 
-    public Subscription getSubscription(String subscriptionId){
-        return null;
+    @Inject
+    private SecurityTokenContainer securityTokenContainer;
+
+    @Inject
+    private MongoDBJsonStore mongoDBJsonStore;
+
+    public JsonArray getAllSubscriptions(){
+        PipelineStore pipelineStore = this.mongoDBJsonStore.getPipelineStore();
+        Tenant tenant = this.securityTokenContainer.getTenant();
+        MongoClient mongoClient = this.mongoDBJsonStore.getMongoClient();
+
+        //Get all subscriptions
+        JsonArray all = pipelineStore.getAllSubscriptions(tenant, mongoClient);
+
+        return all;
     }
 
-    public List<Subscription> getAllSubscriptions(){
-        return null;
+    public Subscription getSubscription(String subscriptionId){
+        PipelineStore pipelineStore = this.mongoDBJsonStore.getPipelineStore();
+        Tenant tenant = this.securityTokenContainer.getTenant();
+        MongoClient mongoClient = this.mongoDBJsonStore.getMongoClient();
+
+        Subscription subscription = pipelineStore.getSubscription(tenant, mongoClient,
+                subscriptionId);
+
+        return subscription;
     }
 
     public void createSubscription(Subscription subscription){
+        PipelineStore pipelineStore = this.mongoDBJsonStore.getPipelineStore();
+        Tenant tenant = this.securityTokenContainer.getTenant();
+        MongoClient mongoClient = this.mongoDBJsonStore.getMongoClient();
 
+        pipelineStore.createSubscription(tenant, mongoClient, subscription);
     }
 
-    public void updateSubscription(Subscription subscription){
+    public Subscription updateSubscription(Subscription subscription){
+        PipelineStore pipelineStore = this.mongoDBJsonStore.getPipelineStore();
+        Tenant tenant = this.securityTokenContainer.getTenant();
+        MongoClient mongoClient = this.mongoDBJsonStore.getMongoClient();
 
+        Subscription updated = pipelineStore.updateSubscription(tenant, mongoClient, subscription);
+
+        return updated;
     }
 
-    public void deleteSubscription(String subscriptionId){
+    public String deleteSubscription(String subscriptionId){
+        PipelineStore pipelineStore = this.mongoDBJsonStore.getPipelineStore();
+        Tenant tenant = this.securityTokenContainer.getTenant();
+        MongoClient mongoClient = this.mongoDBJsonStore.getMongoClient();
 
+        String deleted = pipelineStore.deleteSubscription(tenant,mongoClient,subscriptionId);
+
+        return deleted;
     }
 }
--- src/test/java/com/appgallabs/dataplatform/pipeline/manager/model/SubscriptionTests.java
+++ src/test/java/com/appgallabs/dataplatform/pipeline/manager/model/SubscriptionTests.java
@@ -1,45 +1,182 @@
 package com.appgallabs.dataplatform.pipeline.manager.model;
 
+import com.appgallabs.dataplatform.pipeline.manager.service.SubscriptionService;
+
 import com.appgallabs.dataplatform.util.JsonUtil;
-import com.google.gson.JsonObject;
+import com.google.gson.JsonArray;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import test.components.BaseTest;
 
+import javax.inject.Inject;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @QuarkusTest
-public class SubscriptionTests
+public class SubscriptionTests extends BaseTest
 {
     private static Logger logger = LoggerFactory.getLogger(SubscriptionTests.class);
 
+    @Inject
+    private SubscriptionService subscriptionService;
+
     @Test
-    public void testToJson() throws Exception
+    public void getAllSubscriptions() throws Exception
     {
+        int numberOfSubscriptions = 5;
+
         SubscriberGroup group = new SubscriberGroup();
         group.addSubscriber(new Subscriber("1@1.com"));
         group.addSubscriber(new Subscriber("2@1.com"));
 
         Pipe pushPipe = new Pipe(UUID.randomUUID().toString(),"pipe1");
 
-        Subscription subscription = new Subscription(UUID.randomUUID().toString(),group,pushPipe);
+        for(int i=0; i<numberOfSubscriptions; i++) {
+            Subscription subscription = new Subscription(UUID.randomUUID().toString(), group, pushPipe);
+            this.subscriptionService.createSubscription(subscription);
+        }
 
-        JsonObject json = subscription.toJson();
+        JsonArray all = this.subscriptionService.getAllSubscriptions();
+        assertNotNull(all);
+        JsonUtil.printStdOut(all);
+        assertEquals(numberOfSubscriptions, all.size());
+    }
 
-        JsonUtil.printStdOut(json);
+    @Test
+    public void getSubscription() throws Exception{
+        SubscriberGroup group = new SubscriberGroup();
+        group.addSubscriber(new Subscriber("1@1.com"));
+        group.addSubscriber(new Subscriber("2@1.com"));
 
-        logger.info("***********************************");
-        
-        Subscription deser = Subscription.parse(json.toString());
-        JsonUtil.printStdOut(deser.toJson());
+        Pipe pushPipe = new Pipe(UUID.randomUUID().toString(),"pipe1");
 
-        String originalHash = JsonUtil.getJsonHash(subscription.toJson());
-        String deserHash = JsonUtil.getJsonHash(deser.toJson());
+        List<String> subscriptionIds = new ArrayList<>();
+        List<String> subscriptionHashes = new ArrayList<>();
+        for(int i=0; i<group.getSubscribers().size(); i++) {
+            String subscriptionId = UUID.randomUUID().toString();
+            Subscription subscription = new Subscription(subscriptionId, group, pushPipe);
+            this.subscriptionService.createSubscription(subscription);
 
-        assertEquals(originalHash,deserHash);
+            String hash = JsonUtil.getJsonHash(subscription.toJson());
+            subscriptionIds.add(subscriptionId);
+            subscriptionHashes.add(hash);
+
+            logger.info("****SUBSCRIPTION_HASH_CREATE***");
+            logger.info("ID: " +subscriptionId);
+            logger.info("HASH: " + hash);
+            logger.info("**********************");
+        }
+
+        logger.info("*****************************************************************");
+        for(String subscriptionId: subscriptionIds) {
+            //Get all subscriptions
+            Subscription subscription = this.subscriptionService.getSubscription(subscriptionId);
+
+            String hash = JsonUtil.getJsonHash(subscription.toJson());
+            logger.info("****SUBSCRIPTION_ID_HASH_READ***");
+            logger.info("ID: " +subscriptionId);
+            logger.info("HASH: " + hash);
+            logger.info("**********************");
+
+            assertNotNull(subscription);
+            assertTrue(subscriptionHashes.contains(hash));
+        }
+    }
+
+    @Test
+    public void updateSubscription() throws Exception {
+        SubscriberGroup group = new SubscriberGroup();
+        group.addSubscriber(new Subscriber("1@1.com"));
+        group.addSubscriber(new Subscriber("2@1.com"));
+
+        Pipe pushPipe = new Pipe(UUID.randomUUID().toString(),"pipe1");
+
+        List<String> subscriptionIds = new ArrayList<>();
+        List<String> subscriptionHashes = new ArrayList<>();
+        for(int i=0; i<group.getSubscribers().size(); i++) {
+            String subscriptionId = UUID.randomUUID().toString();
+            Subscription subscription = new Subscription(subscriptionId, group, pushPipe);
+            this.subscriptionService.createSubscription(subscription);
+
+            String hash = JsonUtil.getJsonHash(subscription.toJson());
+            subscriptionIds.add(subscriptionId);
+            subscriptionHashes.add(hash);
+
+            logger.info("****SUBSCRIPTION_HASH_CREATE***");
+            logger.info("ID: " +subscriptionId);
+            logger.info("HASH: " + hash);
+            logger.info("**********************");
+        }
+
+        logger.info("*****************************************************************");
+        for(String subscriptionId: subscriptionIds) {
+            //Get all subscriptions
+            Subscription subscription = this.subscriptionService.getSubscription(subscriptionId);
+            String hash = JsonUtil.getJsonHash(subscription.toJson());
+
+            //update the subscribers
+            String newSubscriber = "3@1.com";
+            subscription.getGroup().addSubscriber(new Subscriber(newSubscriber));
+
+            Subscription updated = this.subscriptionService.updateSubscription(subscription);
+            String newHash = JsonUtil.getJsonHash(updated.toJson());
+            subscriptionHashes.remove(hash);
+            subscriptionHashes.add(newHash);
+
+            logger.info("****SUBSCRIPTION_ID_HASH_UPDATED***");
+            logger.info("ID: " +updated);
+            logger.info("OLD_HASH: " + hash);
+            logger.info("NEW_HASH: " + newHash);
+            JsonUtil.printStdOut(updated.toJson());
+            logger.info("**********************");
+
+            assertNotNull(subscription);
+            assertFalse(subscriptionHashes.contains(hash));
+            assertTrue(subscriptionHashes.contains(newHash));
+        }
+    }
+
+    @Test
+    public void deleteSubscription() throws Exception {
+        SubscriberGroup group = new SubscriberGroup();
+        group.addSubscriber(new Subscriber("1@1.com"));
+        group.addSubscriber(new Subscriber("2@1.com"));
+
+        Pipe pushPipe = new Pipe(UUID.randomUUID().toString(),"pipe1");
+
+        List<String> subscriptionIds = new ArrayList<>();
+        List<String> subscriptionHashes = new ArrayList<>();
+        for(int i=0; i<group.getSubscribers().size(); i++) {
+            String subscriptionId = UUID.randomUUID().toString();
+            Subscription subscription = new Subscription(subscriptionId, group, pushPipe);
+            this.subscriptionService.createSubscription(subscription);
+
+            String hash = JsonUtil.getJsonHash(subscription.toJson());
+            subscriptionIds.add(subscriptionId);
+            subscriptionHashes.add(hash);
+
+            logger.info("****SUBSCRIPTION_HASH_CREATE***");
+            logger.info("ID: " +subscriptionId);
+            logger.info("HASH: " + hash);
+            logger.info("**********************");
+        }
+
+        logger.info("*****************************************************************");
+        for(String subscriptionId: subscriptionIds) {
+            String deleted = this.subscriptionService.deleteSubscription(subscriptionId);
+
+            Subscription subscription = this.subscriptionService.getSubscription(
+                    subscriptionId);
+
+            assertEquals(deleted, subscriptionId);
+            assertNull(subscription);
+        }
     }
 }
