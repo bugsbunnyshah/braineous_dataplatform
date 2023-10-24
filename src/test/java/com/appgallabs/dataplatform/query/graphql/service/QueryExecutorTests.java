738134363758bb005311e8b8e9696efceec28a39
babyboy <bugs.bunny.shah@gmail.com>
2023-10-24 17:25:22 -0400

    pipeline_sdk: graphql components

--- src/test/java/com/appgallabs/dataplatform/query/graphql/service/QueryExecutorTests.java
+++ src/test/java/com/appgallabs/dataplatform/query/graphql/service/QueryExecutorTests.java
@@ -1,10 +1,15 @@
 package com.appgallabs.dataplatform.query.graphql.service;
 
 
+import com.appgallabs.dataplatform.infrastructure.MongoDBJsonStore;
+import com.appgallabs.dataplatform.infrastructure.Tenant;
+import com.appgallabs.dataplatform.preprocess.SecurityTokenContainer;
 import com.appgallabs.dataplatform.util.JsonUtil;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,7 +24,31 @@ public class QueryExecutorTests extends BaseTest {
     private static Logger logger = LoggerFactory.getLogger(QueryExecutorTests.class);
 
     @Inject
+    private MongoDBJsonStore mongoDBJsonStore;
+
+    @Inject
+    private SecurityTokenContainer securityTokenContainer;
+
+    @Inject
     private QueryExecutor queryExecutor;
+
+    @BeforeEach
+    public void setUp() throws Exception{
+        super.setUp();
+        for(int i=0; i<3; i++) {
+            JsonObject jsonObject = new JsonObject();
+            String objectHash = JsonUtil.getJsonHash(jsonObject);
+            jsonObject.addProperty("objectHash", objectHash);
+            jsonObject.addProperty("name", "hello");
+            jsonObject.addProperty("value","value");
+            jsonObject.addProperty("diff",""+i);
+            this.mongoDBJsonStore.storeIngestion(this.securityTokenContainer.getTenant(), jsonObject);
+            Tenant tenant = this.securityTokenContainer.getTenant();
+            JsonObject data = this.mongoDBJsonStore.readEntity(tenant, objectHash);
+            assertNotNull(data);
+            assertEquals(objectHash, data.get("objectHash").getAsString());
+        }
+    }
 
     @Test
     public void executeQueryNoCriteria() throws Exception{
