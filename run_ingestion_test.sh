82d9e760a2809c72842ec3d1226000ac1f188005
babyboy <bugs.bunny.shah@gmail.com>
2023-11-05 01:47:26 -0500

    CR1_release: end-to-end send and receive (hardening)

--- run_ingestion_test.sh
+++ run_ingestion_test.sh
@@ -1,4 +1,5 @@
-mvn clean package -DskipTests && rm ./dataplatform-1.0.0-runner.jar && cp target/dataplatform-1.0.0-runner.jar . && mvn test -Dtest=PipelineServiceTests#ingestObject
+mvn clean package -DskipTests && rm ./dataplatform-1.0.0-runner.jar && cp target/dataplatform-1.0.0-runner.jar .
+&& mvn test -Dtest=PipelineServiceTests#ingestObject
 
 
 
