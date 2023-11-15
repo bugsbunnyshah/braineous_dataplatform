b18b5b8f77d6cbb45f0bea8e7647853ecc2385ae
babyboy <bugs.bunny.shah@gmail.com>
2023-11-15 14:08:49 -0500

    CR1_release: CR1 (hardening)

--- prepare-release-cr1.sh
+++ prepare-release-cr1.sh
@@ -1,3 +1,5 @@
+./build.sh
+
 rm -rf release-cr1
 
 mkdir release-cr1
@@ -15,7 +17,12 @@ cd ..
 cd ..
 
 cp -r releases/braineous-1.0.0-cr1/bin/* release-cr1/braineous-1.0.0-cr1/bin
+cp dataplatform-1.0.0-cr1-runner.jar release-cr1/braineous-1.0.0-cr1/bin/dataplatform-1.0.0-runner.jar
 cp -r tutorials/get-started/* release-cr1/braineous-1.0.0-cr1/tutorials/get-started
+
+cd release-cr1
+zip -r braineous-1.0.0-cr1.zip braineous-1.0.0-cr1
+cd ..
 
 pwd
 
