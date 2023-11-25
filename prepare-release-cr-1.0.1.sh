eb461f11dfde00b695e28b86ac381d19eff30161
babyboy <bugs.bunny.shah@gmail.com>
2023-11-25 14:28:47 -0500

    1.0.1-CR1: tutorial (create_connector)

---
+++ prepare-release-cr-1.0.1.sh
@@ -0,0 +1,30 @@
+./build.sh
+
+rm -rf release-cr-1.0.1
+
+mkdir release-cr-1.0.1
+cd release-cr-1.0.1
+mkdir braineous-1.0.1-cr1
+cd braineous-1.0.1-cr1
+mkdir bin
+mkdir tutorials
+cd tutorials
+mkdir get-started
+mkdir create-connector
+
+
+cd ..
+cd ..
+cd ..
+
+cp -r releases/braineous-1.0.1-cr1/bin/* release-cr-1.0.1/braineous-1.0.1-cr1/bin
+cp dataplatform-1.0.1-cr1-runner.jar release-cr-1.0.1/braineous-1.0.1-cr1/bin/dataplatform-1.0.1-runner.jar
+cp -r tutorials/get-started/* release-cr-1.0.1/braineous-1.0.1-cr1/tutorials/get-started
+
+cd release-cr-1.0.1
+zip -r braineous-1.0.1-cr1.zip braineous-1.0.1-cr1
+cd ..
+
+pwd
+
+
