149bd1b1b95eecaa4ce32a70d7278af3894c82b3
babyboy <bugs.bunny.shah@gmail.com>
2023-12-19 23:23:48 -0500

    release_prep: updating tutorial

--- tutorials/get-started/src/main/java/com/appgallabs/dataplatform/tutorial/DataIngestionTutorial.java
+++ tutorials/get-started/src/main/java/com/appgallabs/dataplatform/tutorial/DataIngestionTutorial.java
@@ -12,8 +12,10 @@ public class DataIngestionTutorial {
     public static void main(String[] args) throws RegisterPipeException, InterruptedException {
         //configure the DataPipeline Client
         Configuration configuration = new Configuration().
-                streamSizeInBytes(80).
-                ingestionHostUrl("http://localhost:8080");
+                ingestionHostUrl("http://localhost:8080/").
+                apiKey("0132d8be-c85c-423a-a168-4767f4dd638b").
+                apiSecret("d8e452ea-9968-434c-b84c-5276781a60b6").
+                streamSizeInBytes(80);
         DataPipeline.configure(configuration);
 
         //setup data pipe configuration json
@@ -22,13 +24,23 @@ public class DataIngestionTutorial {
                 "  \"configuration\": [\n" +
                 "    {\n" +
                 "      \"storeDriver\" : \"com.appgallabs.dataplatform.targetSystem.core.driver.MongoDBStoreDriver\",\n" +
-                "      \"name\": \"get_started_store\",\n" +
+                "      \"name\": \"scenario1_store_mongodb\",\n" +
                 "      \"config\": {\n" +
                 "        \"connectionString\": \"mongodb://localhost:27017\",\n" +
-                "        \"database\": \"get_started_store\",\n" +
-                "        \"collection\": \"data\"\n" +
-                "      },\n" +
-                "      \"jsonpathExpression\": \"jsonpath:1\"\n" +
+                "        \"database\": \"scenario1_store\",\n" +
+                "        \"collection\": \"data\",\n" +
+                "        \"jsonpathExpressions\": []\n" +
+                "      }\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"storeDriver\" : \"com.appgallabs.dataplatform.targetSystem.core.driver.MySqlStoreDriver\",\n" +
+                "      \"name\": \"scenario1_store_mysql\",\n" +
+                "      \"config\": {\n" +
+                "        \"connectionString\": \"jdbc:mysql://localhost:3306/braineous_staging_database\",\n" +
+                "        \"username\": \"root\",\n" +
+                "        \"password\": \"\",\n" +
+                "        \"jsonpathExpressions\": []\n" +
+                "      }\n" +
                 "    }\n" +
                 "  ]\n" +
                 "}";
