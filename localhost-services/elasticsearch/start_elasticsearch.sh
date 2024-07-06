ceb48a80750057f47f8e7955606faa86f8e5a0d3
babyboy <bugs.bunny.shah@gmail.com>
2024-07-06 06:03:36 -0400

    starting_clickhouse_connector_development

---
+++ localhost-services/elasticsearch/start_elasticsearch.sh
@@ -0,0 +1,22 @@
+export ELASTIC_PASSWORD="password"  # password for "elastic" username
+export KIBANA_PASSWORD="password"   # Used _internally_ by Kibana, must be at least 6 characters long
+
+docker run -p 127.0.0.1:9200:9200 -d --name elasticsearch --network elastic-net \
+  -e ELASTIC_PASSWORD=$ELASTIC_PASSWORD \
+  -e "discovery.type=single-node" \
+  -e "xpack.security.http.ssl.enabled=false" \
+  -e "xpack.license.self_generated.type=trial" \
+  docker.elastic.co/elasticsearch/elasticsearch:8.14.2
+
+
+
+docker run -p 127.0.0.1:5601:5601 -d --name kibana --network elastic-net \
+  -e ELASTICSEARCH_URL=http://elasticsearch:9200 \
+  -e ELASTICSEARCH_HOSTS=http://elasticsearch:9200 \
+  -e ELASTICSEARCH_USERNAME=kibana_system \
+  -e ELASTICSEARCH_PASSWORD=$KIBANA_PASSWORD \
+  -e "xpack.security.enabled=false" \
+  -e "xpack.license.self_generated.type=trial" \
+  docker.elastic.co/kibana/kibana:8.14.2
+
+
