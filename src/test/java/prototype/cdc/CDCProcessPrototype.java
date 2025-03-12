2b281e8817d98e0f1a10ed5bf10a15aba83e1de7
babyboy <bugs.bunny.shah@gmail.com>
2025-03-12 11:42:25 -0400

    CDC: implementation : foundation_components : prototype_phase

--- src/test/java/prototype/cdc/CDCProcessPrototype.java
+++ src/test/java/prototype/cdc/CDCProcessPrototype.java
@@ -1,6 +1,8 @@
 package prototype.cdc;
 
+import com.appgallabs.dataplatform.infrastructure.JDBCHelper;
 import com.appgallabs.dataplatform.util.JsonUtil;
+import com.github.wnameless.json.flattener.JsonFlattener;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import org.apache.flink.api.common.functions.MapFunction;
@@ -10,13 +12,19 @@ import org.junit.jupiter.api.Test;
 import test.components.Util;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+import java.util.*;
 
 public class CDCProcessPrototype implements Serializable {
 
     @Test
     public void process() throws Exception{
+        final String configJsonStr = Util.loadResource("cdc/conf/db_config.json");
+        final String[] dataKey = {"name", "email"};
+
         //get the change dataset
         String objStr = Util.loadResource("cdc/obj1_array.json");
         JsonArray objJsonArray = JsonUtil.validateJson(objStr).getAsJsonArray();
@@ -43,7 +51,34 @@ public class CDCProcessPrototype implements Serializable {
             @Override
             public String map(String s) throws Exception {
                 //execute the CDC process/algorithm for each record
-                boolean isInsert = isInsert();
+                JsonObject configJson = JsonUtil.validateJson(configJsonStr).getAsJsonObject();
+
+                Map<String,String> dataKeyMap = new HashMap<>();
+                for(int i=0; i<dataKey.length; i++){
+                    dataKeyMap.put(dataKey[i],dataKey[i]);
+                }
+
+                //flatten
+                JsonObject objJson = JsonUtil.validateJson(s).getAsJsonObject();
+                Map<String, Object> objMap = JsonFlattener.flattenAsMap(objJson.toString());
+
+                //construct a flat object
+                JsonObject flatObject = new JsonObject();
+                Set<Map.Entry<String,Object>> entrySet = objMap.entrySet();
+                for(Map.Entry<String,Object> entry:entrySet){
+                    String key = entry.getKey();
+                    String value = entry.getValue().toString();
+
+                    String field = key;
+                    if(key.indexOf(".") != -1) {
+                        int lastIndex = key.lastIndexOf('.');
+                        field = key.substring(lastIndex+1);
+                    }
+
+                    flatObject.addProperty(field, value);
+                }
+
+                boolean isInsert = isInsert(configJson, dataKey, flatObject);
                 if(isInsert){
                     //insert the record into the live target store
                     insertRecord();
@@ -61,8 +96,44 @@ public class CDCProcessPrototype implements Serializable {
         env.execute();
     }
 
-    private boolean isInsert(){
-        return false;
+    private boolean isInsert(JsonObject configJson, String[] dataKey, JsonObject record) throws Exception{
+        Connection connection = null;
+        Statement statement = null;
+        try{
+            String query = "select * from cdc_test";
+
+            //where clause
+            StringBuilder whereClause = new StringBuilder();
+            for(int i=0; i<dataKey.length; i++){
+                String columnName = dataKey[i];
+                String value = record.get(columnName).getAsString();
+                if(i < dataKey.length-1){
+                    whereClause.append(columnName + "=" + "'" + value + "'" + " AND" + " ");
+                }else{
+                    whereClause.append(columnName + "=" + "'" + value + "'");
+                }
+            }
+
+            query += " " + "where" + " " + whereClause.toString();
+
+            connection = JDBCHelper.getInstance().getConnection(configJson);
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            if(resultSet.next()){
+                //record exists and hence update operation
+                return false;
+            }else{
+                //record does not exist hence insert opertion
+                return true;
+            }
+        }finally{
+            if(connection != null){
+                try{connection.close();}catch(Exception e){}
+            }
+            if(statement != null){
+                try{statement.close();}catch(Exception e){}
+            }
+        }
     }
 
     private void insertRecord(){
