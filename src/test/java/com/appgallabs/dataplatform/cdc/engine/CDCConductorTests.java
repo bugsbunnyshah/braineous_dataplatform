683492d495a64fddb9e76d3b7148c24638f606bd
babyboy <bugs.bunny.shah@gmail.com>
2025-03-17 19:45:14 -0400

    CDC: implementation : foundation_components : productization_phase

--- src/test/java/com/appgallabs/dataplatform/cdc/engine/CDCConductorTests.java
+++ src/test/java/com/appgallabs/dataplatform/cdc/engine/CDCConductorTests.java
@@ -14,6 +14,7 @@ import test.components.Util;
 import javax.inject.Inject;
 
 import java.util.Map;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -27,7 +28,7 @@ public class CDCConductorTests {
     private CDCConductor cdcConductor = new CDCConductor();
 
     @Test
-    public void orchestrate() throws Exception {
+    public void orchestrateInsert() throws Exception {
         String table = "cdc_test";
 
         final String configJsonStr = Util.loadResource("cdc/conf/db_config.json");
@@ -36,6 +37,57 @@ public class CDCConductorTests {
 
         //get the change dataset
         String objStr = Util.loadResource("cdc/insert_array.json");
+        JsonArray objJsonArray = JsonUtil.validateJson(objStr).getAsJsonArray();
+        for(int i=0; i<objJsonArray.size(); i++){
+            Random rand = new Random();
+            long randValue = rand.nextLong();
+
+            JsonObject obj = objJsonArray.get(i).getAsJsonObject().getAsJsonObject("addr");
+            obj.addProperty("email", randValue +"@email.com");
+        }
+        JsonUtil.printStdOut(objJsonArray);
+
+        String query = "select count(*) from " + table;
+        Map<String,String> result = DBDataUtil.query(cdcConfig, query);
+        String beforeCountStr = result.get("count(*)");
+        int beforeCount = 0;
+        if(beforeCountStr != null){
+            beforeCount = Integer.parseInt(beforeCountStr);
+        }
+
+        this.cdcConductor.orchestrate(
+                cdcConfig,
+                objJsonArray
+        );
+
+        Thread.sleep(5000);
+
+
+        //assert the result
+        result = DBDataUtil.query(cdcConfig, query);
+        String afterCountStr = result.get("count(*)");
+        int afterCount = 0;
+        if(afterCountStr != null){
+            afterCount = Integer.parseInt(afterCountStr);
+        }
+
+        System.out.println("***********");
+        System.out.println("BeforeCount: "+beforeCount);
+        System.out.println("AfterCount: "+afterCount);
+        System.out.println("***********");
+        assertTrue(afterCount == (beforeCount+2));
+    }
+
+    @Test
+    public void orchestrateUpdate() throws Exception{
+        String table = "cdc_test";
+
+        final String configJsonStr = Util.loadResource("cdc/conf/db_config.json");
+        JsonObject cdcConfig = JsonUtil.validateJson(configJsonStr).getAsJsonObject();
+        final String[] dataKey = {"name", "email"};
+
+        //get the change dataset
+        String objStr = Util.loadResource("cdc/update_array.json");
         JsonArray objJsonArray = JsonUtil.validateJson(objStr).getAsJsonArray();
         JsonUtil.printStdOut(objJsonArray);
 
@@ -67,6 +119,54 @@ public class CDCConductorTests {
         System.out.println("BeforeCount: "+beforeCount);
         System.out.println("AfterCount: "+afterCount);
         System.out.println("***********");
-        assertTrue(afterCount > beforeCount);
+        assertTrue(afterCount == beforeCount);
+    }
+
+    @Test
+    public void orchestrateBoth() throws Exception{
+        String table = "cdc_test";
+
+        final String configJsonStr = Util.loadResource("cdc/conf/db_config.json");
+        JsonObject cdcConfig = JsonUtil.validateJson(configJsonStr).getAsJsonObject();
+        final String[] dataKey = {"name", "email"};
+
+        //get the change dataset
+        String objStr = Util.loadResource("cdc/both_array.json");
+        JsonArray objJsonArray = JsonUtil.validateJson(objStr).getAsJsonArray();
+        Random rand = new Random();
+        long randValue = rand.nextLong();
+        JsonObject obj = objJsonArray.get(0).getAsJsonObject().getAsJsonObject("addr");
+        obj.addProperty("email", randValue +"@email.com");
+        JsonUtil.printStdOut(objJsonArray);
+
+        String query = "select count(*) from " + table;
+        Map<String,String> result = DBDataUtil.query(cdcConfig, query);
+        String beforeCountStr = result.get("count(*)");
+        int beforeCount = 0;
+        if(beforeCountStr != null){
+            beforeCount = Integer.parseInt(beforeCountStr);
+        }
+
+        this.cdcConductor.orchestrate(
+                cdcConfig,
+                objJsonArray
+        );
+
+        Thread.sleep(5000);
+
+
+        //assert the result
+        result = DBDataUtil.query(cdcConfig, query);
+        String afterCountStr = result.get("count(*)");
+        int afterCount = 0;
+        if(afterCountStr != null){
+            afterCount = Integer.parseInt(afterCountStr);
+        }
+
+        System.out.println("***********");
+        System.out.println("BeforeCount: "+beforeCount);
+        System.out.println("AfterCount: "+afterCount);
+        System.out.println("***********");
+        assertTrue(afterCount == (beforeCount+1));
     }
 }
--- src/test/java/com/appgallabs/dataplatform/cdc/engine/CDCProcessTests.java
+++
@@ -1,5 +0,0 @@
-package com.appgallabs.dataplatform.cdc.engine;
-
-
-public class CDCProcessTests {
-}
